0f6d470b0535f1f96b85b7f08a6352926d8cb507
SrBruni <164874399+SrBruni@users.noreply.github.com>
2024-05-06 13:19:27 +0200

    Update create_projektwoche.sql
    Stand  06.05.24

--- create_projektwoche.sql
+++ create_projektwoche.sql
@@ -3,45 +3,47 @@
 -- Datum: 15.04.24
 
 CREATE TABLE angestellte (
-	kuerzel VARCHAR(4) PRIMARY KEY 
+	kuerzel VARCHAR(4) PRIMARY KEY,
+	a_vorname VARCHAR 
 		NOT NULL,
-	a_vorname VARCHAR (50)
-		NOT NULL,
-	a_nachname VARCHAR (50)
+	a_nachname VARCHAR 
 		NOT NULL
 );
 
 CREATE TABLE schuelerInnen(
 	sus_id INTEGER PRIMARY KEY,
-	sus_vorname 	VARCHAR,
-	sus_nachname 	VARCHAR,
-	Jahrgangsstufe 	INTEGER,
-	profilnummer 	INTEGER,
-	Klassennumer  	INTEGER 
+	sus_vorname VARCHAR
+		NOT NULL,
+	sus_nachname VARCHAR
+		NOT NULL,
+	Jahrgangsstufe 	INTEGER
+		NOT NULL,
+	profilnummer 	INTEGER
+		NOT NULL,
+	Klassennumer  	INTEGER
+		NOT NULL 
 );
 
 CREATE TABLE projekt(
-	pnummer INTEGER PRIMARY KEY,
-	ptitel	VARCHAR(100),
+	pnummer INTEGER PRIMARY KEY UNIQUE, 
+	ptitel	VARCHAR UNIQUE,
 	pleitung VARCHAR (4) not null references angestellte(kuerzel),
 	passitent integer references schuelerInnen(sus_id)
+	
+	--CONSTRAINT projektnummer UNIQUE (pnummer)
 );
 
 CREATE TABLE findet_statt(
 	von_tageszeit TIME, 
 	bis_tageszeit TIME, 
-	wochentag VARCHAR(10),
+	wochentag VARCHAR,
 	check (wochentag in ('montag', 'dienstag', 'mittwoch', 'donnerstag', 'freitag')),
-	raum_oder_ort VARCHAR(50)
+	raum_oder_ort VARCHAR
 );
 
 CREATE TABLE nimmt_teil(
-	pnummer 		INTEGER	REFERENCES projekt, 
-	sus_vorname 	VARCHAR REFERENCES schuelerInnen,
-	sus_nachname 	VARCHAR REFERENCES schuelerInnen,
-	idnummer 		INTEGER REFERENCES schuelerInnen
+	pnummer 		integer references projekt(pnummer),
+	sus_id			integer references schuelerInnen
 );
-
-
 
 
