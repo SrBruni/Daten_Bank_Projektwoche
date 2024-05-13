31d2a716670430b61b5b7677c524dc12d9360634
SrBruni <164874399+SrBruni@users.noreply.github.com>
2024-05-13 13:38:33 +0200

    Add files via upload

---
+++ drop_projektwoche.sql
@@ -0,0 +1,13 @@
+-- Miniwelt Projektwoche
+-- Authoren Bruni, Zumpe
+
+-- Datum: 15.04.24
+
+DROP TABLE angestellte CASCADE;
+DROP TABLE schuelerInnen CASCADE;
+DROP TABLE nimmt_teil CASCADE;
+DROP TABLE leitet CASCADE;
+DROP TABLE leitet_mit CASCADE;
+DROP TABLE projekt CASCADE;
+DROP TABLE findet_statt CASCADE;
+DROP TABLE raum_oder_ort CASCADE;
