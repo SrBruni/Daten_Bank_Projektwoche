94b1107ae7b020546656784cc7c79b7d4d582349
SrBruni <164874399+SrBruni@users.noreply.github.com>
2024-06-17 13:08:07 +0200

    Delete html directory

--- html/index1b.php
+++
@@ -1,9 +0,0 @@
-<form action="f1b.php" method="post" name="f1">
-  <input type="submit" value="zu f1">
-</form>
-<form action="f2b.php" method="post" name="f2">
-  <input type="submit" value="zu f2">
-</form>
-<form action="f1b.php" method="post" name="f1">
-  <input type="submit" value="auch zu f1">
-</form>
